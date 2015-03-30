fd2a0810439788605ed7f2483bd6bc428f25b4ae
Neil Vyas <vyas.neil.nv@gmail.com>
2015-03-29 22:42:13 -0500

    changes to search

--- js/search.js
+++ js/search.js
@@ -2,5 +2,6 @@ SimpleJekyllSearch.init({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('results-container'),
   dataSource: '/search.json',
-  searchResultTemplate: '<li><a href="{url}">{title}</a></li>',
+  searchResultTemplate: '<li><a href="{url}"><strong>{title}</strong> in {type}</a></li>',
+  fuzzy: true,
 })

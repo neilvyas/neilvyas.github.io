1cf816aab21183452b984e3bace19cf16c4a74f3
Neil Vyas <vyas.neil.nv@gmail.com>
2015-12-15 20:58:30 -0600

    iOS fix stuff removed for now

--- js/iOS_fix.js
+++
@@ -1,9 +0,0 @@
-var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
-
-if(iOS){
-	var link = document.createElement("link");
-	link.href = "/css/ios.css";
-	link.type = "text/css";
-	link.rel = "stylesheet";
-	document.getElementsByTagName("head")[0].appendChild(link);
-}

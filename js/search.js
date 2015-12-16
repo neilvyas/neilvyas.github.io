a159799c59711e4be5dc34ed5f283c35e9e2c4ea
Neil Vyas <vyas.neil.nv@gmail.com>
2015-12-15 20:53:54 -0600

    Removing search plugin

--- js/search.js
+++
@@ -1,7 +0,0 @@
-SimpleJekyllSearch.init({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  dataSource: '/search.json',
-  searchResultTemplate: '<li><a href="{url}"><strong>{title}</strong> in {type}</a></li>',
-  fuzzy: true,
-})
